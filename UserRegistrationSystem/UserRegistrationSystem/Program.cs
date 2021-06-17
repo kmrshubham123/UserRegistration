b9e335d3b9dad2dba5f7deb79be73091e31d82ad
kmrshubham123 <kmrshubham123@gmail.com>
2021-06-17 15:25:19 +0530

    [Kumar Shubham]Added:validating User First Name

---
+++ UserRegistrationSystem/UserRegistrationSystem/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace UserRegistrationSystem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("****Welcome to User Registration System****");
+            UserRegistration registration = new UserRegistration();
+            //UC1:-Enter Valid First Name
+            Console.WriteLine(registration.ValidateFirstName("Kumar"));
+        }
+    }
+}
---
+++ UserRegistrationSystem/UserRegistrationSystem/UserRegistration.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace UserRegistrationSystem
+{
+    class UserRegistration
+    {
+        public string REGEX_FIRSTNAME = "^[A-Z][a-z]{2,}$";
+        public bool ValidateFirstName(string firstname)
+        {
+            return Regex.IsMatch(firstname,REGEX_FIRSTNAME);
+        }
+    }
+}
